d7e7236060aeea059786f52e054f95c1834910ef
DESKTOP-HRO9T40\Admin <lenhattuan.it.hcmus@gmail.com>
2020-12-14 13:29:32 +0700

    ajax pagination

--- components/dishes/controller.js
+++ components/dishes/controller.js
@@ -2,13 +2,55 @@ const dishModel = require('./model')
 const userModel = require('../user/model')
 
 exports.index = async (req, res, next) => {
+    if (Object.keys(req.query).length === 0 && req.query.constructor === Object) {
+        let totalDishPerPage = 1
+
+        let totalPage = Math.ceil(await dishModel.totalDish() / (totalDishPerPage * 1.0))
+
+        let dishes = await dishModel.dishlist(1, totalDishPerPage)
+
+        let totalResult = await dishModel.totalDish();
+        let totalPizza = await dishModel.totalPizza();
+        let totalDrink = await dishModel.totalDrink();
+        let totalSide = await dishModel.totalSide();
+
+        let user = await userModel.getUserByUsernameAndPassword('qtt1707', 'qtt1707')
+
+        const dataContext = {
+            menuPageActive: "active",
+            isLogin: true,
+            userID: user.user_id,
+            userFullName: user.name,
+            userAvatar: user.avatar,
+            totalDishPerPageOption1Selected: true,
+            totalDishPerPageOption2Selected: false,
+            totalDishPerPageOption3Selected: false,
+            totalDishPerPageOption4Selected: false,
+            totalResult: totalResult,
+            totalPizza: totalPizza,
+            totalDrink: totalDrink,
+            totalSide: totalSide,
+            isPizzaCatActive: false,
+            isDrinkCatActive: false,
+            isSideCatActive: false,
+            dishes: dishes,
+            totalPage: totalPage,
+            page: 1,
+            category: 0
+        }
+
+        res.render('../components/dishes/views/index', dataContext);
+    } else {
+        this.pagination(req, res, next)
+    }
+}
+
+exports.pagination = async (req, res, next) => {
     const key_name = req.query.key_name
 
     let categoryId = req.query.category;
     let currentPage = req.query.page;
     let totalDishPerPage = req.query.total_dish_per_page;
-
-/*    console.log(req.query)*/
 
     if (categoryId === undefined || categoryId === "")
         categoryId = 0
@@ -19,60 +61,18 @@ exports.index = async (req, res, next) => {
     if (totalDishPerPage === undefined)
         totalDishPerPage = 1
 
-    let totalPage = Math.ceil(await dishModel.totalDish() / (totalDishPerPage * 1.0))
-
-    let isPizzaCatActive = false;
-    let isDrinkCatActive = false;
-    let isSideCatActive = false;
-
     let dishes;
     let totalResult = await dishModel.totalDish();
-    let totalPizza = await dishModel.totalPizza();
-    let totalDrink = await dishModel.totalDrink();
-    let totalSide = await dishModel.totalSide();
-
-    let totalDishPerPageOption1Selected = false;
-    let totalDishPerPageOption2Selected = false;
-    let totalDishPerPageOption3Selected = false;
-    let totalDishPerPageOption4Selected = false;
-
-    switch (totalDishPerPage) {
-        case '1':
-            totalDishPerPageOption1Selected = true;
-            break;
-        case '2':
-            totalDishPerPageOption2Selected = true;
-            break;
-        case '3':
-            totalDishPerPageOption3Selected = true;
-            break;
-        case '4':
-            totalDishPerPageOption4Selected = true;
-            break;
-    }
+    let totalPage = Math.ceil(await dishModel.totalDish() / (totalDishPerPage * 1.0))
 
     if (key_name !== undefined) {
         dishes = await dishModel.searchByKeyName(key_name)
-        totalResult = dishes.length
     } else {
         if (categoryId !== 0) {
             dishes = await dishModel.listByCategory(categoryId, currentPage, totalDishPerPage)
             totalResult = await dishModel.totalDishByCategory(categoryId);
+
             totalPage = Math.ceil(totalResult / (totalDishPerPage * 1.0))
-
-            switch (categoryId) {
-                case '1':
-                    isPizzaCatActive = true;
-                    break;
-
-                case '2':
-                    isDrinkCatActive = true;
-                    break;
-
-                case '3':
-                    isSideCatActive = true;
-                    break;
-            }
         } else {
             dishes = await dishModel.dishlist(currentPage, totalDishPerPage)
         }
@@ -80,32 +80,14 @@ exports.index = async (req, res, next) => {
 
     let admin1 = await userModel.getUserByUsernameAndPassword('qtt1707', 'qtt1707')
 
-    const dataContext = {
-        menuPageActive: "active",
-        isLogin: true,
-        userID: admin1.user_id,
-        userFullName: admin1.name,
-        userAvatar: admin1.avatar,
-        totalDishPerPageOption1Selected: totalDishPerPageOption1Selected,
-        totalDishPerPageOption2Selected: totalDishPerPageOption2Selected,
-        totalDishPerPageOption3Selected: totalDishPerPageOption3Selected,
-        totalDishPerPageOption4Selected: totalDishPerPageOption4Selected,
-        totalResult: totalResult,
-        totalPizza: totalPizza,
-        totalDrink: totalDrink,
-        totalSide: totalSide,
-        isPizzaCatActive: isPizzaCatActive,
-        isDrinkCatActive: isDrinkCatActive,
-        isSideCatActive: isSideCatActive,
-        dishes: dishes,
+    const data = {
+        category: categoryId,
+        currentPage: currentPage,
         totalPage: totalPage,
-        page: currentPage,
-        category: categoryId
+        dishes: dishes
     }
 
-/*    console.log(dataContext)*/
-
-    res.render('../components/dishes/views/index', dataContext);
+    res.send(data)
 }
 
 exports.detail = async (req, res, next) => {
@@ -157,7 +139,6 @@ exports.detail = async (req, res, next) => {
         toppings: toppings,
         sizes: sizes,
         images: images,
-        menuPageActive: "active",
         dishes: dish
     }
 
