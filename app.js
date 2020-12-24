9779353831ba75fe3b3308b3e4164b3c19732742
THUẬN TRƯƠNG QUỐC <18120583@student.hcmus.edu.vn>
2020-12-24 11:12:18 +0700

    add check out ui

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ require('./components/dishes/helper')(hbs);
 const userRouter = require('./components/user/router');
 const homeRouter = require('./components/home/router');
 const dishesRouter = require('./components/dishes/router');
+const cartRouter = require('./components/cart/router')
 
 //test
 const app = express();
@@ -31,6 +32,7 @@ app.use('/', homeRouter);
 app.use('/home', homeRouter);
 app.use('/dishes', dishesRouter);
 app.use('/user-profile', userRouter);
+app.use('/cart', cartRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
